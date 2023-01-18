b246b1708984be2999233e8bee1acdd9ad707c86
Sandra Sarpong <90385305+SandraSarpong@users.noreply.github.com>
2023-01-18 20:55:17 +0000

    hash tables

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,46 +1,34 @@
 #!/usr/bin/env python3
-"""
-a python module to obtain the HTML content of a particular
-URL & returns it
-"""
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
+from typing import Callable
 from functools import wraps
 
+redis = redis.Redis()
 
-def cache_page(method: callable) -> callable:
-    """
-    cache_page - function to cache a given url
-    Arguments:
-        the given function
-    Returns:
-        the function passed as argument
-    """
-    obj = redis.Redis()
 
-    @wraps(method)
-    def wrapper(args):
-        """ a wrapper function to return a page &
-        increment the number of times the page has accessed
-        """
-        obj.incr("count:{}" + args)
-        in_cache = obj.get("cached_page" + args)
-        if in_cache:
-            return in_cache.decode("utf-8")
-        html = method(args)
-        obj.set("cached_page:" + args, html, 10)
-        return html
+def wrap_requests(fn: Callable) -> Callable:
+    """ Decorator wrapper """
+
+    @wraps(fn)
+    def wrapper(url):
+        """ Wrapper for decorator guy """
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
+        return result
+
     return wrapper
 
 
-@cache_page
+@wrap_requests
 def get_page(url: str) -> str:
+    """get page self descriptive
     """
-    get_page - function to get page & returns it
-    Arguments:
-        url: the given url
-    Returns:
-        the obtained html page
-    """
-    html = requests.get(url).text
-    return html
+    response = requests.get(url)
+    return response.text
